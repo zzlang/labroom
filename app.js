6ad1525547d0934f6b514ebd5ac0c885d751febf
lyj <lyj@ubuntu.(none)>
2011-12-15 10:35:11 +0800

    test

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var app=express.createServer();
 //test();
 app.use(express.static(__dirname+"/static"));
 
+
+
+
 app.get("/",function(req,res,next){
   res.render("mobile.jade",{layout:false});
 });
