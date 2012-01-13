32eb65b30f143479f653727d20fca4013a10e9d8
zzlang <ilovey2@gmail.com>
2012-01-13 16:19:29 +0800

    test html5 video

--- app.js
+++ app.js
@@ -2,12 +2,19 @@ var express=require("express");
 var app=express.createServer();
 
 //test();
-app.use(express.static(__dirname+"/static"));
+app.use("/static",express.static(__dirname+"/static"));
+app.set('views', __dirname + '/views');
+app.register(".html", require("stache"));
 
 
 
 
 app.get("/",function(req,res,next){
+  app.set("view engine",'jade');
   res.render("mobile.jade",{layout:false});
 });
+app.get("/video",function(req,res,next){
+  app.set("view engine",'mustache');
+  res.render("video.html",{layout:false});
+})
 app.listen(4444);
